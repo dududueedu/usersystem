5041b81076a00e3bd342e1188a2b602fd0e266d9
Eduardo Gomes <eduardooffside@gmail.com>
2022-01-18 01:00:35 -0300

    first test-api: post

--- backend/src/main/java/com/dev/usersystem/controller/UserController.java
+++ backend/src/main/java/com/dev/usersystem/controller/UserController.java
@@ -6,6 +6,8 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
 
 import com.dev.usersystem.model.User;
@@ -24,6 +26,16 @@ public class UserController {
 			return new ResponseEntity<List<User>>(getUsers, HttpStatus.OK);
 		}else {
 			return new ResponseEntity<>("No users available!", HttpStatus.NOT_FOUND);
+		}
+	}
+	
+	@PostMapping("/users")
+	public ResponseEntity<?> createUser(@RequestBody User user){
+		try {
+			userRepo.save(user);
+			return new ResponseEntity<User>(user, HttpStatus.OK);
+		} catch (Exception err) {
+			return new ResponseEntity<>(err.getMessage(), HttpStatus.INTERNAL_SERVER_ERROR);
 		}
 	}
 }
--- backend/src/main/java/com/dev/usersystem/model/User.java
+++ backend/src/main/java/com/dev/usersystem/model/User.java
@@ -23,4 +23,45 @@ public class User {
 	private String email;
 	
 	private String telefone;
+	
+	public User() {}
+	
+	public User(String id, String nome, String email, String telefone) {
+		this.id = id;
+		this.nome = nome;
+		this.email = email;
+		this.telefone = telefone;
+	}
+
+	public String getId() {
+		return id;
+	}
+
+	public void setId(String id) {
+		this.id = id;
+	}
+
+	public String getNome() {
+		return nome;
+	}
+
+	public void setNome(String nome) {
+		this.nome = nome;
+	}
+
+	public String getEmail() {
+		return email;
+	}
+
+	public void setEmail(String email) {
+		this.email = email;
+	}
+
+	public String getTelefone() {
+		return telefone;
+	}
+
+	public void setTelefone(String telefone) {
+		this.telefone = telefone;
+	}
 }
