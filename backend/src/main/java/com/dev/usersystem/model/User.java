86e36e4850940db76780e5586bee6a51299f994e
Eduardo Gomes <eduardooffside@gmail.com>
2022-01-20 15:30:00 -0300

    upgrade: classUser

--- backend/src/main/java/com/dev/usersystem/model/User.java
+++ backend/src/main/java/com/dev/usersystem/model/User.java
@@ -4,12 +4,8 @@ import org.springframework.data.annotation.Id;
 import org.springframework.data.mongodb.core.mapping.Document;
 
 import lombok.AllArgsConstructor;
-import lombok.Getter;
 import lombok.NoArgsConstructor;
-import lombok.Setter;
 
-@Setter
-@Getter
 @AllArgsConstructor
 @NoArgsConstructor
 @Document(collection = "users")
